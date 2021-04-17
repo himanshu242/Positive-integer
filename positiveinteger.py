f867580800019e1cf5d7f8a1a3be2c91be6935f2
himanshu242 <82162502+himanshu242@users.noreply.github.com>
2021-04-17 14:59:08 +0530

    Create positiveinteger.py
    Done BY:-Himanshu kishor marathe

---
+++ positiveinteger.py
@@ -0,0 +1,16 @@
+#for list1 
+
+list1=[12,-7,5,64,-14]
+
+for number in list1:
+    if number>=0:
+        print(number,end= " ")
+        
+#for list2
+
+list2=[12,14,-95,3]
+a=[]
+for number in list2:
+    if number>=0:
+        a.append(number)
+        print(a, end=" ")
